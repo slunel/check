f309565afc97625beaffcd24c93e0bd10fd9a59e
LUNEL Simon <simon.lunel@soprasteria.com>
2022-02-09 18:19:08 +0100

    add readable position

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,6 +4,7 @@ object Main extends App {
   println("Hello, you are playing the whites" )
 
   val posFrom: Position = RetrievePosFrom()
+  println("you have chosen the position " + PositionUtils.PositionToString(posFrom) + " at coordinates " + posFrom.x.toString + posFrom.y.toString)
   val posTo: Position = RetrievePosTo()
 
   val piece: Piece = new Piece(posFrom)
@@ -11,28 +12,19 @@ object Main extends App {
 
 
 
-  // Move to a new location
-
   def RetrievePosFrom():Position = {
-    println("Please input the x position of the piece you want to move")
-    val x = scala.io.StdIn.readInt()
-    println("You have chosen the x position: " + x)
-    println("Please input the y position of the piece you want to move")
-    val y = scala.io.StdIn.readInt()
-    println("You have chosen the y position: " + y)
-    new Position(x,y)
+    println("Please input the position of the piece you want to move")
+    val input = scala.io.StdIn.readLine()
+    PositionUtils.StringToPosition(input)
   }
+
+
 
   def RetrievePosTo():Position = {
-    println("Please input the x position where you want to move")
-    val x = scala.io.StdIn.readInt()
-    println("You have chosen the x position: " + x)
-    println("Please input the y position where you want to move")
-    val y = scala.io.StdIn.readInt()
-    println("You have chosen the y position: " + y)
-    new Position(x,y)
+    println("Please input the position where you want to move")
+    val input = scala.io.StdIn.readLine()
+    PositionUtils.StringToPosition(input)
   }
-
 
 }
 
@@ -43,6 +35,27 @@ class Position(val xc: Int, val yc: Int) {
 
   def isLegalPosition(): Boolean = {
     x > 0 && x < 9 && y > 0 && y < 9
+  }
+}
+
+object PositionUtils {
+  def StringToPosition(input: String):Position = {
+    val y:Int = input(1).asDigit
+    val x:Int = input(0) match {
+      case 'a' => 1
+      case 'b' => 2
+    }
+    new Position(x,y)
+  }
+
+  def PositionToString(position: Position):String = {
+    val x: Int = position.x
+    val xString: String = x match {
+      case 1 => "a"
+      case 2 => "b"
+    }
+    val y: String = position.y.toString
+    xString + y
   }
 }
 
